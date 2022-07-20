1042a8f4d43dd400c6aab8e31d4e543a10f94292
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-07-20 17:20:08 -0400

    add demo build scripts

--- sample-node-tsc/web.ts
+++ sample-node-tsc/web.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello, world');
+  res.send('hello, world2');
 });
 
 app.listen(process.env.PORT || 8080);
